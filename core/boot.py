7c231b4c7b5779c2ceb12664870a1462c5b67596
Adam Gordon Bell <adam@corecursive.com>
2023-06-27 12:33:48 -0400

    clean up

--- core/boot.py
+++ core/boot.py
@@ -1,19 +1,7 @@
 from Crypto.Cipher import AES
 import os
-import base64
 import dotenv
 
-try:
-    dotenv.load_dotenv()
-    cipher = AES.new(os.getenv('TO_EARTHLY_AST')[:32].encode(), AES.MODE_ECB)
-    script_dir = os.path.dirname(os.path.abspath(__file__))
-    full_filepath = f"{script_dir}/../data/data"
-    with open(full_filepath, 'r') as file:
-        encrypted_code = file.read()
-
-    padded_code = cipher.decrypt(base64.b64decode(encrypted_code)).decode()
-    padding_length = ord(padded_code[-1])
-    original_code = padded_code[:-padding_length]
-    exec(original_code)
-except Exception as e:
-    dotenv.load_dotenv()
+dotenv.load_dotenv()
+if os.environ.get('OPENAI_API_KEY') is None:
+    raise EnvironmentError('OPENAI_API_KEY ENV is not set.')
